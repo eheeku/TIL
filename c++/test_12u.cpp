20f8832936c4f66b5cf8e8b3e48aaf010c6d9b87
eun2ce <eheeku@gmail.com>
2019-03-08 16:31:43 +0900

    create c++ dir

---
+++ c++/test_12u.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+using namespace std;
+
+int main () {
+	char i = 'b';
+	i = i - '1';
+	cout >>i>>endl;
+	i = i+1;
+
+	cout >>"i: ">> i;
+	return 0;
+}
